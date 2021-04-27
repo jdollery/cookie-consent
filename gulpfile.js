f6b715a2142bd660ac62b5a27d09bfc4b416bc86
jdollery <54100876+jdollery@users.noreply.github.com>
2021-04-27 14:55:36 +0100

    Using gulp to inject html into js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 // npm init
 // npm install --save-dev gulp
-// npm install node-sass gulp-sass gulp-concat gulp-uglify gulp-sourcemaps --save-dev
 // npm install node-sass gulp-sass gulp-sourcemaps --save-dev
+// npm install gulp-concat gulp-uglify --save-dev
+// npm install gulp-inject-views --save-dev
 
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
-// const concat = require('gulp-concat');
-// const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const inject = require('gulp-inject-views');
 
 
 const paths = {
@@ -16,16 +18,11 @@ const paths = {
     src: './src/scss/**/*.scss',
     dest: './dist'
   },
-  // scripts: {
-  //   src: [
-  //     './src/js/jquery.js',
-  //     './src/js/navigation.js',
-  //     './src/js/validate.js',
-  //     './src/js/slick.js',
-  //     './src/js/script.js'
-  //   ],
-  //   dest: './dist'
-  // }
+  injects: {
+    src: './src/js/cookie-consent.js',
+    html: './src/html/**/*.html',
+    dest: './dist'
+  }
 };
 
 	
@@ -41,20 +38,21 @@ function css() {
 exports.css = css;
 
 
-// function js() {
-//   return gulp
-//   .src(paths.scripts.src)
-//   .pipe(concat('script.js'))
-//   .pipe(uglify())
-//   .pipe(gulp.dest(paths.scripts.dest))
-// }
+function injection() {
+  return gulp
+  .src(paths.injects.src)
+  .pipe(inject(paths.injects.html))
+  .pipe(concat('cookie-consent.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest(paths.injects.dest))
+}
 
-// exports.js = js;
+exports.inject = injection;
 
 
 function watch() {
   gulp.watch(paths.styles.src, css);
-  // gulp.watch(paths.scripts.src, js);
+  gulp.watch(paths.injects.src, injection);
 }
 	
 exports.watch = watch;
---
+++ src/js/cookie-consent.js
@@ -0,0 +1,78 @@
+const Cookieconsent = function init() {
+
+  //https://thisinterestsme.com/add-html-body-javascript/
+  //https://stackoverflow.com/questions/13495010/how-to-add-content-to-html-body-using-js
+
+  //Create a container for the dialog
+  const cookieHTML = document.createElement("div");
+
+  //Give the container unique id
+  cookieHTML.id = 'cookieConsent';
+
+  //Give the container unique id
+  // cookieHTML.classList.add("cookie-consent");
+
+  //Add dialog and reset to container - insertAdjacentHTML is a quicker and backward compatable, rather than innerHTML
+  cookieHTML.insertAdjacentHTML("beforeend", "{{load path='./src/html/dialog.html'}}"); //inject with gulp
+  
+  //Append the container to the HTML body
+  document.body.appendChild(cookieHTML);
+
+  const dialog = document.getElementById("cookieDialog");
+  const allowButton = document.getElementById("allowCookies");
+  const declineButton = document.getElementById("declineCookies");
+  const resetButton = document.getElementById("resetCookies");
+
+  //Find cookie and value
+  window.getCookie = function(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(let i=0;i < ca.length;i++) {
+      let c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
+
+  //Accept cookies and close dialog
+  allowButton.addEventListener('click', function() {
+    let now = new Date();
+    let time = now.getTime();
+    let expireTime = time + 180*24*60*60*1000; //expire in 6 months
+    now.setTime(expireTime);
+    document.cookie = 'cookieConsent' + '=true; Path=/; Expires=' + now.toGMTString(); //add cookieConsent cookie & set to ture
+    window.location.reload();
+  });
+
+  //Reject cookies and close dialog
+  declineButton.addEventListener('click', function() {
+    document.cookie = 'cookieConsent' + '=false; Path=/;'; //set cookieConsent cookie to false
+    window.location.reload();
+  });
+
+  //Open dialog
+  resetButton.addEventListener('click', function() {
+    dialog.classList.add("cookie-dialog--active"); 
+  });
+
+  const x = getCookie('cookieConsent')
+  
+  if (!x) {
+    dialog.classList.add("cookie-dialog--active"); //Show dialog if no actioned
+
+  } else if (x == 'true') {
+    console.log('Cookies Allowed');
+
+    dialog.classList.remove("cookie-dialog--active"); //Remove dialog if actioned
+
+  } else if (x == 'false') {
+    console.log('Cookies Declined');
+
+    dialog.classList.remove("cookie-dialog--active"); //Remove dialog if actioned
+
+  }
+
+};
+
+Cookieconsent();
